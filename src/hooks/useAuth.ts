cd2e12280ca5ace55879a09207345328c2298f4f
Muhammad Rinaldi <muhammad.rinaldi007.mr@gmail.com>
2025-05-05 12:33:58 +0800

    Delete beberapa masalah

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,12 +1,9 @@
 "use client";
 
-import { useAuthStore } from "@/stores/authStore";
 import { loginUser } from "@/services/authService";
 import { destroyCookie, setCookie } from "nookies";
 
 export function useAuth() {
-  const setToken = useAuthStore((state) => state.setToken);
-
   async function login(email: string, password: string) {
     const token = await loginUser(email, password);
 
@@ -15,12 +12,9 @@ export function useAuth() {
       maxAge: 7 * 24 * 60 * 60, // 7 hari
       path: "/",
     });
-
-    setToken(token);
   }
   function logout() {
     destroyCookie(null, "token");
-    setToken("");
   }
 
   return { login, logout };
